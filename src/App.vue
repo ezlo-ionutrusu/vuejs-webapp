bc13e8d05d27e9b4244fd3c730ede0721c07bf30
ezlo-ionutrusu <ionut.rusu@ezlo.com>
2019-05-20 14:25:48 +0300

    styled native app

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,32 @@
 </template>
 <template native>
   <Page>
-    <Label textWrap="true" text="Play with NativeScript demo!"/>
+    <ActionBar title="MyApp"/>
+    <ScrollView>
+      <StackLayout class="home-panel">
+        <!--Add your page content here-->
+        <Label textWrap="true" text="Play with NativeScript!" class="h2 description-label"/>
+        <Label
+          textWrap="true"
+          text="Write code in the editor or drag and drop components to build a NativeScript mobile application."
+          class="h2 description-label"
+        />
+        <Label
+          textWrap="true"
+          text="Scan the QR code with your mobile device and watch the changes sync live while you play with the code."
+          class="h2 description-label"
+        />
+        <Button text="Button" @tap="onButtonTap"/>
+      </StackLayout>
+    </ScrollView>
   </Page>
 </template>
 <script>
 import MainPage from '@/main/containers/Page/Index.vue';
 import { getAuthProvider } from '@/API/';
 
-const { VUE_APP_MODE } = process.env;
+// const { VUE_APP_MODE } = process.env;
+const { alert } = require('tns-core-modules/ui/dialogs');
 
 export default {
   components: {
@@ -21,6 +39,18 @@ export default {
   mounted() {
     // eslint-disable-next-line no-console
     getAuthProvider().auth.connect().then((response) => { console.log(response); });
+  },
+  methods:
+  {
+    onButtonTap() {
+      alert({
+        title: 'Your title',
+        message: 'Your message',
+        okButtonText: 'Your button text',
+      }).then(() => {
+        console.log('The user closed the alert.');
+      });
+    },
   },
 };
 </script>
